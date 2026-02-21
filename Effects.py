b269bb1cfdf61a972e4e89888a0c6e5e33f7b136
NicolleIdaM <nicolleidamuller@gmail.com>
2026-02-21 19:42:05 -0300

    Creating menu

--- Effects.py
+++ Effects.py
@@ -6,3 +6,13 @@ capture = cv2.VideoCapture(0)
 if not capture.isOpened():
     print("Erro: Não foi possível acessar a webcam!")
     exit()
+
+effect = 0
+
+print("EFFECTS")
+print("1 - Normal")
+print("2 - Gray scale")
+print("3 - Border detection")
+print("4 - Negative")
+print("0 - Exit")
+
